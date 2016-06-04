60f36f82755c5cba9390c2687c67bda88c504c58
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-04 10:58:02 +0300

    [Chore] Update app __init__ file

--- app/__init__.py
+++ app/__init__.py
@@ -1,29 +1,13 @@
 from flask import Flask
-from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from config import config
 
-bootstrap = Bootstrap()
 db = SQLAlchemy()
 
-login_manager = LoginManager()
-login_manager.session_protection = 'strong'
-login_manager.login_view = 'auth.login'
 
-
-def create_app(config_name):
+def create_app(configuration):
     app = Flask(__name__)
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
-    app.config.from_object(config[config_name])
-    app.config['SECRET_KEY'] = 'you-will-never-guess'
-    config[config_name].init_app(app)
+    app.config.from_object(configuration)
 
-    bootstrap.init_app(app)
     db.init_app(app)
-    login_manager.init_app(app)
-
-    with app.app_context():
-        db.create_all()
 
     return app
