85507d02f9fc726234d0b6cb8ba05052ba004014
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-04 10:57:40 +0300

    [Chore] Update main executable file

--- app.py
+++ app.py
@@ -1,26 +1,11 @@
-#!/usr/bin/env python
-import os
-from app import create_app, db
-from app.models import User, Bucketlist, Item
-from flask.ext.script import Manager, Shell
-from flask.ext.migrate import Migrate, MigrateCommand
-
-app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
-migrate = Migrate(app, db)
+from app import create_app
+from config import DevelopmentConfig
+app = create_app(DevelopmentConfig)
 
 
-def make_shell_context():
-    '''Returns application and database instances
-    to the shell importing them automatically
-    on `python manager.py shell`.
-    '''
-    return dict(app=app, db=db, User=User, Bucketlist=Bucketlist, Item=Item)
-
-
-manager.add_command("shell", Shell(make_context=make_shell_context))
-manager.add_command('db', MigrateCommand)
-
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
-    manager.run()
+    app.run()
