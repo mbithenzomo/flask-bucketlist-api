ff559e62172c82d60f38d1c462efe8f961bbc5cd
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-15 17:38:05 +0300

    [Chore] Add app instance to app.py file

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-from app import create_app
-from config import DevelopmentConfig
-app = create_app(DevelopmentConfig)
+from app.version1.resources import Index, UserLogin, UserRegister, \
+    BucketListsAPI, BucketListAPI, ItemsAPI, ItemAPI
+from app import api, app
 
-
-@app.route('/')
-def hello():
-    return "Hello World!"
+""" Defining the API resources """
+api.add_resource(Index, '/')
+api.add_resource(UserLogin, '/auth/login/')
+api.add_resource(UserRegister, '/auth/register/')
+api.add_resource(BucketListsAPI, '/bucketlists/')
+api.add_resource(BucketListAPI, '/bucketlist/<id>')
+api.add_resource(ItemsAPI, '/bucketlists/<id>/items/')
+api.add_resource(ItemAPI, '/bucketlists/<id>/items/<item_id>')
 
 if __name__ == '__main__':
     app.run()
