7e915b031667516b09f8dbae2cfcd710d2030570
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-15 17:41:49 +0300

    [Chore] Create app instance in app/__init__.py file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,18 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.restful import Api
+from config.config import app_config
 
 db = SQLAlchemy()
 
 
 def create_app(configuration):
     app = Flask(__name__)
-    app.config.from_object(configuration)
+    app.config.from_object(app_config[configuration])
 
     db.init_app(app)
 
     return app
+
+app = create_app("development")
+api = Api(app=app, prefix="/api/v1.0")
