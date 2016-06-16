ae0511d0e031d164a3b032d3d2a1b9a0cc6db456
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-16 11:36:24 +0300

    [Fix] Remove unique constraint from title field in bucket list model

--- app/models.py
+++ app/models.py
@@ -69,7 +69,7 @@ class Bucketlist(db.Model):
     __tablename__ = "bucketlists"
 
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(70), unique=True,)
+    title = db.Column(db.String(70))
     description = db.Column(db.Text)
     date_added = db.Column(db.DateTime, default=datetime.utcnow)
     date_edited = db.Column(db.DateTime, default=datetime.utcnow)
