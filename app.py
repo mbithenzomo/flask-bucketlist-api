0c024e9fc250f50becc1444ec3c83a84923f5cda
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-16 15:57:35 +0300

    [Fix] Edit API resource endpoints

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ api.add_resource(Index, '/')
 api.add_resource(UserLogin, '/auth/login/')
 api.add_resource(UserRegister, '/auth/register/')
 api.add_resource(BucketListsAPI, '/bucketlists/')
-api.add_resource(BucketListAPI, '/bucketlist/<id>')
+api.add_resource(BucketListAPI, '/bucketlists/<id>')
 api.add_resource(ItemsAPI, '/bucketlists/<id>/items/')
 api.add_resource(ItemAPI, '/bucketlists/<id>/items/<item_id>')
 
