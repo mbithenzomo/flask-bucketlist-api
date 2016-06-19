1c6a81d63b455c7a6a3498f63108757cc2102677
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-19 14:32:11 +0300

    [Fix] Modify path to test database

--- config/config.py
+++ config/config.py
@@ -3,43 +3,44 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    """Default configurations """
+    """ Default configurations """
 
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(basedir, 'bucketlist.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + \
+        os.path.join(basedir, "bucketlist.db")
 
 
 class DevelopmentConfig(Config):
-    """Development configurations """
+    """ Development configurations """
 
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
-        os.path.join(basedir, 'bucketlist.db')
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + \
+        os.path.join(basedir, "bucketlist.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "p9Bv<3Eid9%$i01"
 
 
 class TestingConfig(Config):
-    """Test configurations """
+    """ Test configurations """
 
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///test.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + \
+        os.path.join(basedir, "test.db")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
-    """Production configurations """
+    """ Production configurations """
 
     DEBUG = False
     TESTING = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///models/bucketlist.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///models/bucketlist.db"
 
 app_config = {
-    'development': DevelopmentConfig,
-    'testing': TestingConfig,
-    'production': ProductionConfig
+    "development": DevelopmentConfig,
+    "testing": TestingConfig,
+    "production": ProductionConfig
 }
