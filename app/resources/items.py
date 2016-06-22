a4fac0d7e4fb5f85a82a4ddde6b1a5d4657430c1
Mbithe Nzomo <mbithe.nzomo@andela.com>
2016-06-22 22:52:18 +0300

    [Chore] Add decorator to Item methods to check whether user is authorized to access the item(s) requested

--- app/resources/items.py
+++ app/resources/items.py
@@ -6,26 +6,47 @@ from .. models import Bucketlist, Item
 from base import unauthorized, add_item, delete_item, edit_item
 
 
+def authorized_user_bucketlist(function):
+    def auth_wrapper(*args, **kwargs):
+        g.bucketlist = Bucketlist.query.filter_by(id=kwargs["id"]).first()
+        try:
+            if g.bucketlist.created_by == g.user.id:
+                return function(*args, **kwargs)
+            return unauthorized()
+        except:
+            return unauthorized("Error: The bucket list specified doesn't "
+                                "exist. Please try again!")
+    return auth_wrapper
+
+
+def authorized_user_item(function):
+    def auth_wrapper(*args, **kwargs):
+        g.item = Item.query.filter_by(bucketlist_id=kwargs["id"],
+                                      id=kwargs["item_id"]).first()
+        try:
+            if g.item.created_by == g.user.id:
+                return function(*args, **kwargs)
+            return unauthorized()
+        except:
+            return unauthorized("Error: The bucket list item specified "
+                                "doesn't exist. Please try again!")
+    return auth_wrapper
+
+
 class ItemsAPI(Resource):
     """
     URL: /api/v1/bucketlists/<id>/items/
     Request methods: GET, POST
     """
+    @authorized_user_bucketlist
     def get(self, id):
         """ Get all items in a bucket list """
-        bucketlist = Bucketlist.query.get(id)
-        if bucketlist:
-            if bucketlist.created_by == g.user.id:
-                items = Item.query.filter_by(bucketlist_id=id).all()
-                if items:
-                    return marshal(items, item_serializer)
-                return {"message": "The bucket list sepcified has no items. "
-                        "Add one and try again!"}
-            else:
-                return unauthorized()
-        else:
-            return unauthorized("Error: The bucket list specified does not "
-                                "exist. Please try again!")
+        items = Item.query.filter_by(bucketlist_id=id).all()
+        try:
+            return marshal(items, item_serializer)
+        except:
+            return {"message": "The bucket list specified has no items. "
+                    "Add one and try again!"}
 
     def post(self, id):
         """ Add a new item to a bucket list """
@@ -61,75 +82,39 @@ class ItemAPI(Resource):
     URL: /api/v1/bucketlists/<id>/items/<item_id>
     Request methods: GET, PUT, DELETE
     """
+    @authorized_user_item
+    @authorized_user_bucketlist
     def get(self, id, item_id):
         """ Get a bucket list item """
-        bucketlist = Bucketlist.query.get(id)
-        if bucketlist:
-            item = Item.query.filter_by(bucketlist_id=id,
-                                        id=item_id).first()
-            if item:
-                if item.created_by == g.user.id:
-                    return marshal(item, item_serializer)
-                else:
-                    return unauthorized()
-            else:
-                return unauthorized("Error: The bucket list item specified "
-                                    "does not exist. Please try again!")
-        else:
-            return unauthorized("Error: The bucket list specified does not "
-                                "exist. Please try again!")
+        return marshal(g.item, item_serializer)
 
+    @authorized_user_item
+    @authorized_user_bucketlist
     def put(self, id, item_id):
         """ Edit a bucket list item """
-        bucketlist = Bucketlist.query.get(id)
-        if bucketlist:
-            item = Item.query.filter_by(bucketlist_id=id,
-                                        id=item_id).first()
-            if item:
-                if item.created_by == g.user.id:
-                    parser = reqparse.RequestParser()
-                    parser.add_argument("title",
-                                        required=True,
-                                        help="No title provided.")
-                    parser.add_argument("description", type=str, default="")
-                    args = parser.parse_args()
-                    title, description = args["title"], args["description"]
-                    item.title = title
-                    item.description = description
-                    item.bucketlist_id = id
-                    return edit_item(name="title",
-                                     item=item,
-                                     serializer=item_serializer,
-                                     is_user=False,
-                                     is_bucketlist=False,
-                                     is_item=True)
-                else:
-                    return unauthorized()
-            else:
-                return {"message": "The bucket list item you are trying to "
-                        "edit does not exist. Please try again!"}
-        else:
-            return unauthorized("Error: The bucket list specified does not "
-                                "exist. Please try again!")
+        parser = reqparse.RequestParser()
+        parser.add_argument("title",
+                            required=True,
+                            help="No title provided.")
+        parser.add_argument("description", type=str, default="")
+        args = parser.parse_args()
+        title, description = args["title"], args["description"]
+        g.item.title = title
+        g.item.description = description
+        g.item.bucketlist_id = id
+        return edit_item(name="title",
+                         item=g.item,
+                         serializer=item_serializer,
+                         is_user=False,
+                         is_bucketlist=False,
+                         is_item=True)
 
+    @authorized_user_item
+    @authorized_user_bucketlist
     def delete(self, id, item_id):
         """ Delete a bucket list item """
-        bucketlist = Bucketlist.query.filter_by(id=id).first()
-        if bucketlist:
-            item = Item.query.filter_by(bucketlist_id=id,
-                                        id=item_id).first()
-            if item:
-                if item.created_by == g.user.id:
-                    return delete_item(item,
-                                       item.title,
-                                       is_user=False,
-                                       is_bucketlist=False,
-                                       is_item=True)
-                else:
-                    return unauthorized()
-            else:
-                return {"message": "The bucket list item you are trying to "
-                        "delete does not exist. Please try again!"}
-        else:
-            return unauthorized("Error: The bucket list specified does not "
-                                "exist. Please try again!")
+        return delete_item(g.item,
+                           g.item.title,
+                           is_user=False,
+                           is_bucketlist=False,
+                           is_item=True)
